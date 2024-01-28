9eb0d708f55a3ea653ce6f7f645f0edca221d612
briant♦♣♠♥ muller <bmuller18@gmail.com>
2024-01-27 22:41:51 -0500

    Muestra pokedex en pantalla

--- pokedex.py
+++ pokedex.py
@@ -1,20 +1,31 @@
 import requests
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from pokedex import *
 
-def obtener_pokemon(nombre):
-    url = f"http://pokeapi.co/api/v2/pokemon/{nombre}"
-    respuesta = requests.get(url)
-    pokemon = respuesta.json()
-    return pokemon
+#Obtiene informacion del pokemon desde la api
+class Pokedex(ttk.Frame):
+    def __init__(self, master, nombre_pokemon):
+        super().__init__(master, width=800, height=600)
+        self.pokemon = self.obtener_pokemon(nombre_pokemon)
+
+    def obtener_pokemon(self, nombre):
+        url = f"http://pokeapi.co/api/v2/pokemon/{nombre}"
+        respuesta = requests.get(url)
+        if respuesta.status_code == 200:
+            pokemon = respuesta.json()
+            return pokemon
+        else:
+            print(f"No se encontró el Pokémon: {nombre}")
+            return None
 
 
+if __name__ == "__main__":
+    root = ttk.Window("Pokedex", themename="superhero")
+    pokedex = Pokedex(root, "pikachu")
 
-pokemon = obtener_pokemon("pikachu")
-
-claves_pokemon = pokemon.keys()
-
-abilities = pokemon.items()
-
-print(abilities)
-
-for claves in claves_pokemon:
-    print(claves)
+    pokelbl = ttk.Label(root, text="Name: " + str(pokedex.pokemon["name"]))
+    pokelbl.pack()
+    
+    pokedex.pack(fill=BOTH, expand=YES)
+    root.mainloop()
